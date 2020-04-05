7841adaa93325d3b8a6ef87352bae88de48d4dd8
xiahua <xiahua@meicai.cn>
2020-04-05 21:59:32 +0800

    test

---
+++ tests/nlp_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   nlp_test.py
+@Time    :   2020/04/05 21:06:40
+@Author  :   xiahua007 
+@Version :   1.0
+@Contact :   xiahua007@qq.com
+'''
+import jieba
+
+print(jieba.lcut("我是中华人民共和国的一名合法公民"))
